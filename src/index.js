087d0b80e40b29eae565b79e3754365038f02a67
Oleksii83 <zaaytav@gmail.com>
2022-07-20 22:34:51 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    console.log(n);
+    let res = n.toString().split("").reverse().join("");
+    console.log(res.slice(0, -1));
+
+    if (res.slice(-1) == "-") {
+        return Number(res.slice(0, -1));
+    }
+
+    if (res.slice(0, 1) == "-") {
+        return Number(res.slice(1));
+    } else {
+        return Number(res);
+    }
+};
